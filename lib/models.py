b2f98f5f388fb69fc34e051da3bdbe2703268a44
Ronny Nyabuto <ronnyabuto@icloud.com>
2026-02-28 22:58:50 +0300

    Restore emojis to output messages to match autotest requirements

--- lib/models.py
+++ lib/models.py
@@ -9,7 +9,7 @@ class Task:
 
     def complete(self):
         self.completed = True
-        print(f"Task '{self.title}' completed.")
+        print(f"✅ Task '{self.title}' completed.")
 
 # TODO: Define the User class
 # Each user has a name and a list of tasks
@@ -22,7 +22,7 @@ class User:
 
     def add_task(self, task):
         self.tasks.append(task)
-        print(f"Task '{task.title}' added to {self.name}.")
+        print(f"📌 Task '{task.title}' added to {self.name}.")
 
     def get_task_by_title(self, title):
         for task in self.tasks:
